8f4ed28ab2c46490cf96b8f017d876d9b31406ec
kore-labs <57886899+kore-labs@users.noreply.github.com>
2020-01-14 12:34:34 -0800

    Update db_link.php

--- php/dbu/db_link.php
+++ php/dbu/db_link.php
@@ -4,34 +4,34 @@
 class db_link{
 
 
-public static function open_link($database)
-{
-	//Initialize Variables
-	$dbname =  "thirdpla_studentux";
-	$user = "thirdpla_ux";
-	$pass = "V4b5KcbZ9[J.";
-	$host = "localhost";
+	public static function open_link($database)
+	{
+		//Initialize Variables
+		$dbname =  "thirdpla_studentux";
+		$user = "thirdpla_ux";
+		$pass = "V4b5KcbZ9[J.";
+		$host = "localhost";
 
-	$link = mysqli_connect($host, $user, $pass, $dbname) or die("Error " . mysqli_error($link));
+		$link = mysqli_connect($host, $user, $pass, $dbname) or die("Error " . mysqli_error($link));
 
-	return $link;
-
-
-}
-
-public static function open_con()
-{
-	$dbname =  "thirdpla_studentux";
- 	$user = "thirdpla_ux";
- 	$pass = "V4b5KcbZ9[J.";
- 	$host = "localhost";
-
-	$mysqli = new mysqli($host, $user, $pass, $dbname);
-
-	return $mysqli;
+		return $link;
 
 
-}
+	}
+
+	public static function open_con()
+	{
+		$dbname =  "thirdpla_studentux";
+		$user = "thirdpla_ux";
+		$pass = "V4b5KcbZ9[J.";
+		$host = "localhost";
+
+		$mysqli = new mysqli($host, $user, $pass, $dbname);
+
+		return $mysqli;
+
+
+	}
 
 
 }
