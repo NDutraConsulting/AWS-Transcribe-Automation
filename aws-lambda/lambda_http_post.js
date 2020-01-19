e2047ea815b802069049b02282477beeead0a306
kore-labs <57886899+kore-labs@users.noreply.github.com>
2020-01-19 15:11:15 -0800

    Update lambda_http_post.js

--- aws-lambda/lambda_http_post.js
+++ aws-lambda/lambda_http_post.js
@@ -3,7 +3,10 @@ const http = require('http');
 exports.handler = async (event, context) => {
   //Does not like the pure ip address
   let url = "ec2-18-220-105-175.us-east-2.compute.amazonaws.com";
-  let path = "/test_reciever.php";//?"+json;//'/lambda_start_transcribe.php'; //
+  let path = "/testing/test_reciever.php"; //Used for debugging and testing
+  
+  //Live endpoint 
+  //lambda_start_transcribe.php'; //
 
 
   let json = "json="+JSON.stringify(event);
@@ -15,13 +18,12 @@ exports.handler = async (event, context) => {
             path: path,
             port: 80,
             method: 'POST',
-              headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(postData)
-  }
-        };
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Length': Buffer.byteLength(postData)
+            }
+        }; //END of options object
 
-        //console.log(options);
 
         const req = http.request(options, (res) => {
           resolve('Success');
@@ -31,7 +33,9 @@ exports.handler = async (event, context) => {
           reject(e.message);
         });
 
+        //Debug
         //console.log(postData);
+      
         // send the request
         req.write(postData);
         req.end();
