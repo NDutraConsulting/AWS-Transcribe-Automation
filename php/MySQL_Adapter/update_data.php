2ed2b04f1a68fa3ba7adea7691e939e55e832647
kore-labs <57886899+kore-labs@users.noreply.github.com>
2020-01-19 15:04:21 -0800

    Update update_data.php

--- php/MySQL_Adapter/update_data.php
+++ php/MySQL_Adapter/update_data.php
@@ -11,163 +11,148 @@
 #               to manage updates needed when database standards change.
 #               EX: Inserts should bind data.
 #
-# Integration into NCANDA System : run from any file that needs it
+# Integration into Student UX System : run from any file that needs it
 #
 # Nikko Dutra Bouck
 # Updated: Nov 3rd 2018
 #
 #
-/* Example:
- $update_data = new update_data();
- $sql = "REPLACE INTO ".tables::current_customer_web_view()." ( customer_id, timestamp, current_view) VALUES( '$customer_id', '".time()."','$current_view' );";
- $update_data->run_query_with_string($sql);
-*/
+/** Usage Example Code:
+ | $update_data = new update_data();
+ | $sql = "REPLACE INTO ".tables::current_customer_web_view()." ( customer_id, timestamp, current_view) VALUES( '$customer_id', '".time()."','$current_view' );";
+ | $update_data->run_query_with_string($sql);
+**/
 #-------------------------------------------------------------------------------
-
 
 include_once "/home/thirdpla/public_html/studentux/php/pathways.php";
 
-
-//include_once debug_code_absolute_path;
 require_once database_link_absolute_path;
 
-
 class update_data{
+	
   private $DEBUG_ON = false;
 
-	private $tablename;
+  private $tablename;
 
-	public $data;
+  public $data;
 
-	public function __construct( )
-	{
+  public function __construct( ){
+  }
 
-		//echo $tablename. $get_parameters. $where_parameters;
 
+  public function run_query_with_string_and_debug($q, $DEBUG){
+
+	$mysqli= db_link::open_con();
+
+
+	if (!$mysqli->query($q)) {
+
+		if( $this->DEBUG_ON || $DEBUG){
+			// Oh no! The query failed.
+			echo "Sorry, the website is experiencing problems.</br>";
+
+			// Again, do not do this on a public site, but we'll show you how
+			// to get the error information
+			echo "Error: Our query failed to execute and here is why: </br>";
+			echo "Query: " . $q . "</br>";
+			echo "Errno: " . $mysqli->errno . "</br>";
+			echo "Error: " . $mysqli->error . "</br>";
+			//exit;
+		}
+
+		$mysqli->close();
+		return false;
 	}
 
+	// The script will automatically free the result and close the MySQL
+	// connection when it exits, but let's just do it anyways
+	//$result->free();
+	$mysqli->close();
+	return true;
 
-    public function run_query_with_string_and_debug($q, $DEBUG){
-
-  		$mysqli= db_link::open_con();
-
-
-  		if (!$mysqli->query($q)) {
-
-  				if( $this->DEBUG_ON || $DEBUG){
-  					// Oh no! The query failed.
-  					echo "Sorry, the website is experiencing problems.</br>";
-
-  					// Again, do not do this on a public site, but we'll show you how
-  					// to get the error information
-  					echo "Error: Our query failed to execute and here is why: </br>";
-  					echo "Query: " . $q . "</br>";
-  					echo "Errno: " . $mysqli->errno . "</br>";
-  					echo "Error: " . $mysqli->error . "</br>";
-  					//exit;
-  			}
-
-        $mysqli->close();
-        return false;
-  		}
-
-
-  		// The script will automatically free the result and close the MySQL
-  		// connection when it exits, but let's just do it anyways
-  		//$result->free();
-  		$mysqli->close();
-      return true;
-
-
-  	}
-
+  }
 
   public function run_query_with_string($q){
 
-		$mysqli= db_link::open_con();
+	$mysqli= db_link::open_con();
 
 
-		if (!$mysqli->query($q)) {
+	if (!$mysqli->query($q)) {
 
-				if( $this->DEBUG_ON ){
-					// Oh no! The query failed.
-					echo "Sorry, the website is experiencing problems.</br>";
+		if( $this->DEBUG_ON ){
+			// Oh no! The query failed.
+			echo "Sorry, the website is experiencing problems.</br>";
 
-					// Again, do not do this on a public site, but we'll show you how
-					// to get the error information
-					echo "Error: Our query failed to execute and here is why: </br>";
-					echo "Query: " . $q . "</br>";
-					echo "Errno: " . $mysqli->errno . "</br>";
-					echo "Error: " . $mysqli->error . "</br>";
-					//exit;
-			}
-
-		}
-
-
-		// The script will automatically free the result and close the MySQL
-		// connection when it exits, but let's just do it anyways
-		//$result->free();
-		$mysqli->close();
-
+			// Again, do not do this on a public site, but we'll show you how
+			// to get the error information
+			echo "Error: Our query failed to execute and here is why: </br>";
+			echo "Query: " . $q . "</br>";
+			echo "Errno: " . $mysqli->errno . "</br>";
+			echo "Error: " . $mysqli->error . "</br>";
+			//exit;
+		}	
 
 	}
 
-	public function run_query( $tablename, $set_parameters, $where_parameters ){
-
-    //Start the query string
-    $q = "UPDATE ". $tablename." SET ".$set_parameters ;
-
-    //Insert the Where parameters
-    if( $where_parameters != 0 ){
-
-    	$q .= " WHERE ".$where_parameters;
-    }
-
-    //Close the Query
-    $q .= ";";
-
-    //echo "<br> $q </br>";
-
-		//Open Mysqli connection
-		$mysqli= db_link::open_con();
+	// The script will automatically free the result and close the MySQL
+	// connection when it exits, but let's just do it anyways
+	//$result->free();
+	$mysqli->close();
 
 
-    if (!$result = $mysqli->query($q)) {
+  }
 
-        if( $this->DEBUG_ON ){
-          // Oh no! The query failed.
-          echo "Sorry, the website is experiencing problems.";
+  public function run_query( $tablename, $set_parameters, $where_parameters ){
 
-          // Again, do not do this on a public site, but we'll show you how
-          // to get the error information
-          echo "Error: Our query failed to execute and here is why: \n";
-          echo "Query: " . $sql . "\n";
-          echo "Errno: " . $mysqli->errno . "\n";
-          echo "Error: " . $mysqli->error . "\n";
-          exit;
-      }
-      return "NO DATA";
-    }
+	//Start the query string
+	$q = "UPDATE ". $tablename." SET ".$set_parameters ;
 
-        //echo $q;
-    $this->data = Array();
+	//Insert the Where parameters
+	if( $where_parameters != null ){
+		$q .= " WHERE ".$where_parameters;
+	}
 
-    while ($row = $result->fetch_assoc()) {
-        // Get each entry returned
-        $this->data[] = $row;
-    }
+	//Close the Query
+	$q .= ";";
 
-		return $this->data;
-
-    // The script will automatically free the result and close the MySQL
-    // connection when it exits, but let's just do it anyways
-    $result->free();
-    $mysqli->close();
+	//Open Mysqli connection
+	$mysqli= db_link::open_con();
 
 
+	if (!$result = $mysqli->query($q)) {
 
-    }
+		if( $this->DEBUG_ON ){
+		  // Oh no! The query failed.
+		  echo "Sorry, the website is experiencing problems.";
+
+		  // Again, do not do this on a public site, but we'll show you how
+		  // to get the error information
+		  echo "Error: Our query failed to execute and here is why: \n";
+		  echo "Query: " . $sql . "\n";
+		  echo "Errno: " . $mysqli->errno . "\n";
+		  echo "Error: " . $mysqli->error . "\n";
+		  exit;
+		}
+      		return "NO DATA";
+    	}
+
+    	$this->data = Array();
+
+	while ($row = $result->fetch_assoc()) {
+		// Get each entry returned
+		$this->data[] = $row;
+	}
+
+
+	// The script will automatically free the result and close the MySQL
+	// connection when it exits, but let's just do it anyways
+	$result->free();
+	$mysqli->close();
+
+	return $this->data;
+
+
+  } #End Run Query	
 
 
 
